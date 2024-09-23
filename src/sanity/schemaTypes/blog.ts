a31d6c1cba9aa5ca2abf59ef2d55824d22da95d3
Muhammad Shahzaib Anwar <m.shahzaibanwar33@gmail.com>
2024-09-23 05:17:24 +0500

    SanityIntegration

---
+++ src/sanity/schemaTypes/blog.ts
@@ -0,0 +1,42 @@
+import { defineField, defineType } from 'sanity'
+
+export default defineType({
+  name: 'blog',
+  title: 'Blog',
+  type: 'document',
+  fields: [
+    defineField({
+      name: 'title',
+      title: 'Title',
+      type: 'string',
+    }),
+    defineField({
+      name: 'slug',
+      title: 'Slug',
+      type: 'slug',
+      options: {
+        source: 'title',
+        maxLength: 96,
+      },
+    }),
+    defineField({
+      name: 'body',
+      title: 'Body',
+      type: 'text',
+    }),
+    defineField({
+      name: 'image',
+      title: 'Image',
+      type: 'image',
+      options: {
+        hotspot: true,
+      },
+    }),
+    defineField({
+      name: 'author',
+      title: 'Author',
+      type: 'reference',
+      to: [{ type: 'author' }],
+    }),
+  ],
+})
